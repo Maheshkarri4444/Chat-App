5ab18d5edda26662af3d6ad56a3dd95bd96ac2ce
Mahesh Karri <157581288+Maheshkarri4444@users.noreply.github.com>
2024-11-09 00:04:44 +0530

    Update Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import LogoutButton from './LogoutButton'
 
 const Sidebar = () => {
   return (
-    <div className='min-h-[65vh] max-h-[65vh] sm:max-h-[100vh] w-[60vw] min-w-[360px] sm:w-full flex flex-col p-4 pb-10 border-r border-slate-500'>
+    <div className='min-h-[65vh] max-h-[65vh] sm:max-h-[100vh] w-[60vw] min-w-[380px] sm:w-full flex flex-col p-4 pb-10 border-r border-slate-500'>
         <SearchInput/>
         <div className='px-3 sm:divider'></div>
         <Conversations/>
